75d150fd5b8cae1e932bc5624624615795cb6e27
Kateryna Redkina <redkinakate@gmail.com>
2023-05-29 19:41:52 -0400

    final version

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Show Loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete(){    
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 //FROM API file
 // let apiQuotes = [];
 
@@ -26,10 +45,45 @@
 
 //FROM LOCAL by using quotes.js file
 function newQuote() {
+    loading();
    //Pick a random quote from API Quotes array
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-     console.log(quote);
+    //console.log(quote);
+    //authorText.textContent = quote.author;
+    //quoteText.textContent = quote.text;
+
+    //Check if Author field is blank and replace it with 'Unknown'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }
+    else{
+        authorText.textContent = quote.author;
+    }
+
+    //Check Quote length to determine styling
+    if(quote.text.length > 30){
+        quoteText.classList.add('long-quote')
+    }
+    else{
+        quoteText.classList.remove('long-quote') 
+    }
+    //Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
 }
+
+//Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText
+    .textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote);
 
 //On Load
 newQuote();
+
+//loading();
